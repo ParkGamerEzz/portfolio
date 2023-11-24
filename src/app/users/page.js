bc5d9b578ee48c602e893af453ec5401af7d0abd
ParkGamerEzz <parkggezdc@gmail.com>
2023-11-24 20:39:52 +0700

    test2

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,25 +1,74 @@
 "use client";
-import React, { useEffect } from 'react';
-import { mockUsers } from '@/constants';
+import axios from 'axios';
+
+import React, { useEffect, useState } from 'react';
+// import { mockUsers } from '@/constants';
 import { CardUser } from '@/components';
 import './App.css';
 
 const Page = () => {
+    const [mockUsers, setMockUsers] = useState([]);
+    async function sendPostRequest(url, params) {
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: params,
+            });
+    
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+    
+            const jsonResponse = await response.json();
+            return jsonResponse;
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+    const getuser = async () => {
+        try {
+            const response = await axios.post('https://mas.acr.ac.th/mas.studentdb/route/routes.asp', {
+                params: 'page_move=get-student-desc-by-idcode&param=17685&param=1',
+            }, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            });
+    
+            const data = response.data;
+            console.log(data);
+            setMockUsers(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
     useEffect(() => {
-        console.log(usermap);
+        getuser()
     }, []);
 
-    const usermap = mockUsers.map((e) => `${e.name} ${e.age} ${e.email} ${e.address}`);
+    useEffect(() => {
+        console.log(mockUsers)
+    }, [mockUsers]);
 
-    const showCard = (name, email, age, address) => (
-        <CardUser name={name} email={email} age={age} address={address} />
+    // const usermap = mockUsers.map((e) => `${e.name} ${e.age} ${e.email} ${e.address}`);
+
+    const showCard = (index, name, email, age, address) => (
+        <CardUser key={index} name={name} email={email} age={age} address={address} />
     );
 
     return (
         <div className='flex flex-wrap gap-4 justify-center align-middle mt-[150px]'>
-            {mockUsers.map((user) => {
+
+
+
+
+            {mockUsers.map((user, index) => {
                 return (
-                    showCard(user.name, user.email, user.age, user.address)
+                    showCard(index, user.name, user.email, user.username, user.address.city)
                 )
             }
 
